9aebb85939d9384b406910f7a38323075372b350
gguggu <48512207+gguggu@users.noreply.github.com>
2019-07-17 07:40:07 +0900

    [FIX]  find 오류 수정

--- WEB/src/routes/Find/Find.js
+++ WEB/src/routes/Find/Find.js
@@ -8,30 +8,67 @@ import PropertyList from './LostProperty/PropertyList';
 class Find extends Component {
     constructor(props) {
         super(props);
-        this.state=[];
+        this.state={
+            department: "",
+            triangleCoords: [
+
+            ],
+        }
       }
 
       //
-    async componentDidMount(){
+    async componentWillMount(){
         const { map } = this.props.store;
         await map.getMap();
-        await map.getPollygon();
+    }
+
+     getPlaceId = async () => {
+        const { map } = this.props.store;
+        // const { department } = this.state;
+        await map.getPlaceId(this.state.department);
+    }
+
+    changeDepartment = (e) =>{
+        console.log(e.target.value);
+        this.setState({
+            department: e.target.value
+        })
+    }
+
+    getPollygons = async (triangleCoords) =>{
+        const { map } = this.props.store;
+        const pollygons = await map.getPollygon();
+        console.log(pollygons);
+
+        return(
+            <Polygon
+            paths={triangleCoords}
+            strokeColor="#FF0000"
+            strokeOpacity={0.8}
+            strokeWeight={2}
+            fillColor='#FF0000'
+            fillOpacity={0.35}
+        />
+        )
     }
 
     render() {
-        // const triangleCoords = [
-        //     {lat: 35, lng: 128},
-        //     {lat: 35, lng: 129},
-        //   ];
+        const triangleCoords = [
+            {lat: 35, lng: 128},
+            {lat: 35, lng: 129},
+          ];
 
-        const { pollygon } = this.props.store.map.pollygons;
-        console.log(pollygon);
-        const triangleCoords = [];
-          console.log(this.props.google);
+        // const { maps } = this.props.store.map;
+        // console.log(this.props.google);
+        // console.log(maps);
         return (
             <div>
                 <div>
                     <input type="text"/>
+                </div>
+                <div>
+                    주소<input type="text" onChange={(e) => this.changeDepartment(e)}/>
+                    <input type="button" value="보내기" onClick={() => this.getPlaceId()}/>
                 </div>
                 <PropertyList/>
                 <Map 
@@ -41,31 +78,11 @@ class Find extends Component {
                     initialCenter={{
                     lat: 37,
                     lng: 126
-                }}
+                    }}
+                    onGoogleApiLoaded={({map, maps}) => this.getPollygons(triangleCoords)}
+                    yesIWantToUseGoogleMapApiInternals={true}
                 >
-                {
-                    this.props.store.map.pollygons.map(e => {
-                        return(
-                            <Polygon
-                                paths={triangleCoords}
-                                strokeColor="#FF0000"
-                                strokeOpacity={0.8}
-                                strokeWeight={2}
-                                fillColor='#FF0000'
-                                fillOpacity={0.35}
-                            />
-                        )
-                    })
-                }
-                 {/* <Polygon
-                                paths={triangleCoords}
-                                strokeColor="#FF0000"
-                                strokeOpacity={0.8}
-                                strokeWeight={2}
-                                fillColor='#FF0000'
-                                fillOpacity={0.35}
-                            /> */}
-                    </Map>
+                </Map>
             </div>
         );
     }
--- WEB/src/store/MapStore/MapStore.js
+++ WEB/src/store/MapStore/MapStore.js
@@ -6,13 +6,13 @@ import { autobind } from 'core-decorators';
 class MapStore {
     @observable maps = {};
     @observable pollygons = [];
+    @observable placeId = "";
 
    @action async getMap(){
        try {
            const data = await MapRepository.getMap();
            console.log(data);
-
-           this.maps = data.data;
+           this.maps = data;
            console.log(this.maps);
        } catch (err) {
            console.log(err);
@@ -21,7 +21,7 @@ class MapStore {
 
    @action async getPollygon(){
        try {
-           const data = await MapRepository.getPollygon();
+           const data = await MapRepository.getPollygon(this.placeId);
            this.pollygons = data.data.data;
            console.log(data);
            console.log(this.pollygons);
@@ -30,9 +30,10 @@ class MapStore {
        }
    }
 
-   @action async getPlaceId(){
+   @action async getPlaceId(Idx){
        try {
-           
+           const data = await MapRepository.getPlaceId(Idx);
+           this.placeId = data.data.data[0].place_id;
        } catch (err) {
            console.log(err);
        }
