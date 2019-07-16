d57877a8d31f8882218dc445298a158b4b233343
wowjdd133 <39159879+wowjdd133@users.noreply.github.com>
2019-07-16 14:28:12 +0900

    Update Login.js

--- WEB/src/routes/Login/Login.js
+++ WEB/src/routes/Login/Login.js
@@ -1,36 +1,68 @@
-import React, { Component } from 'react';
+import React, {
+    Component
+} from 'react';
+import axios from 'axios';
 
 class Login extends Component {
-    state= {
+    state = {
         userId: "",
         password: ""
     }
 
     handleChange = (e) => {
-        let nextState = {};
-        nextState[e.target.name] = e.target.value;
-        this.setState(nextState);
+        this.setState({
+            [e.target.name]: e.target.value,
+        })
+    }
+
+    handleSubmit = async (e) => {
+        const {
+            userId: id,
+            password: pw
+        } = this.state;
+        await axios.post('http://192.168.137.1:7777/auth/login', {
+              id,
+              pw})
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
 
     render() {
-        return (
-            <div>
-                <label> ID</label>
-                <input  type="text"
-                        name="userId"
-                        placeholder="User Id"
-                        value={this.state.userId}
-                        onChange={this.handleChange}/>
-                
-                <label> Password</label>
-                <input  type="password"
-                        name="password"
-                        placeholder="Password"
-                        value={this.state.password}
-                        onChange={this.handleChange}/>
-                
-                <button type="button"> Sign in </button>
-            </div>
+        return ( 
+          <div>
+            <label> ID </label> 
+            <input  type = "text"
+                    name = "userId"
+                    placeholder = "User Id"
+                    value = {
+                      this.state.userId
+                    }
+                    onChange = {
+                        this.handleChange
+                    }
+                    />
+
+            <label> Password </label> 
+            <input  type = "password"
+                    name = "password"
+                    placeholder = "Password"
+                    value = {
+                        this.state.password
+                    }
+                    onChange = {
+                        this.handleChange
+                    }
+                    />
+
+            <button type = "button"
+                    onClick = {
+                      this.handleSubmit
+                    }> Sign in </button> 
+          </div>
         );
     }
 }
